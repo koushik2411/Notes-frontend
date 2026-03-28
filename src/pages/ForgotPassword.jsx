c61741a9e6c79c8d3a1a09e49620de7966c12130
koushik2411 <koushikrk1324@gmail.com>
2026-03-28 22:56:36 +0530

    Forgot password page added

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import API from "../services/api";
 import { useNavigate, Link } from "react-router-dom";
+import { FaUserEdit } from "react-icons/fa";
 
 export default function ForgotPassword() {
     const [email, setEmail] = useState("");
@@ -22,7 +23,8 @@ export default function ForgotPassword() {
     };
 
     return (
-        <div className=" w-full min-h-screen flex flex-col gap-5 items-center justify-center">
+        <div className=" w-full min-h-screen flex flex-col gap-5 items-center justify-center bg-slate-200 text-slate-900 dark:bg-slate-900 dark:text-slate-100">
+        <div><FaUserEdit className=" text-9xl"/></div>
             <form className=" flex flex-col gap-4">
                 <div className=" flex flex-col gap-1">
                     <label>Email</label>
@@ -30,7 +32,7 @@ export default function ForgotPassword() {
                         type="email"
                         placeholder="e.g., ninjahattori@gmail.com"
                         onChange={e => setEmail(e.target.value)}
-                        className=" px-3 py-2 border rounded"
+                        className=" px-3 py-2 border rounded-lg placeholder:text-slate-500 w-[70vw] max-w-175"
                     />
                 </div>
                 <div className=" flex flex-col gap-1">
@@ -39,11 +41,12 @@ export default function ForgotPassword() {
                         type="password"
                         placeholder="********"
                         onChange={e => setNewPassword(e.target.value)}
-                        className=" px-3 py-2 border rounded"
+                        className=" px-3 py-2 border rounded-lg placeholder:text-slate-500 w-[70vw] max-w-175"
                     />
                 </div>
                 <button
-                onClick={handleReset}>
+                onClick={handleReset}
+                className=" bg-linear-to-b from-slate-800 to-slate-900 text-slate-100 dark:bg-linear-to-b  dark:from-blue-500 dark:to-blue-600 rounded-lg p-3 my-2 mb-8 font-semibold">
                     Set new password
                 </button>
             </form>
