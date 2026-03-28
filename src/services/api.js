a0ddff19e10f674f164f4da3f913657742362c33
Koushik Kumar <koushikrk1324@gmail.com>
2026-03-28 22:32:30 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://notes-backend-atum.onrender.com"
+    baseURL: "https://notes-backend-atum.onrender.com/"
 });
 
 API.interceptors.request.use((req) => {
